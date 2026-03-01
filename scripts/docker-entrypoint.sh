57658e1022940093fadeaf211a634b46e0dd32bf
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2026-03-02 01:55:13 +0530

    updated triggerfile code

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,17 +1,2 @@
 #!/bin/sh
-set -eu
-
-TRIGGER_SCHEDULE="${TRIGGER_SCHEDULE:-0 */6 * * *}"
-OUTPUT_DIR="${OUTPUT_DIR:-/wallpapers}"
-CRON_FILE="/etc/crontabs/root"
-
-mkdir -p "$OUTPUT_DIR"
-
-# Run once on startup so the volume is populated immediately.
-/usr/local/bin/bing_wallpaper_downloader.sh
-
-printf '%s %s >> /proc/1/fd/1 2>> /proc/1/fd/2\n' \
-  "$TRIGGER_SCHEDULE" \
-  "/usr/local/bin/bing_wallpaper_downloader.sh" > "$CRON_FILE"
-
-exec crond -f -l 8
+exec /usr/local/bin/bing_wallpaper_downloader.sh
